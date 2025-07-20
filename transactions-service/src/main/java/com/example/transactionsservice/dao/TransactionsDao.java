1768ed6bf70917e0825f0c418bc44b5dc448c894
noureldeen73 <nourie2003@gmail.com>
2025-07-20 17:21:57 +0300

    Creating TransactionDao

---
+++ transactions-service/src/main/java/com/example/transactionsservice/dao/TransactionsDao.java
@@ -0,0 +1,21 @@
+package com.example.transactionsservice.dao;
+
+import com.example.transactionsservice.model.Transaction;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TransactionsDao {
+
+    void createTransaction(Transaction transaction);
+
+    void TransactionSuccess(UUID transactionId);
+
+    void TransactionFailed(UUID transactionId);
+
+    List<Transaction> getTransactionsByAccountId(UUID accountId);
+
+    List<Transaction> getAllTransactions();
+
+    Transaction getTransactionById(UUID transactionId);
+}
---
+++ transactions-service/src/main/java/com/example/transactionsservice/dao/TransactionsDaoImpl.java
@@ -0,0 +1,61 @@
+package com.example.transactionsservice.dao;
+
+
+import com.example.transactionsservice.model.Transaction;
+import com.example.transactionsservice.model.TransactionStatus;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class TransactionsDaoImpl implements TransactionsDao{
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Override
+    public void createTransaction(Transaction transaction) {
+        entityManager.persist(transaction);
+        entityManager.flush();
+
+    }
+
+    @Override
+    public void TransactionSuccess(UUID transactionId) {
+        Transaction transaction = entityManager.find(Transaction.class, transactionId);
+        if (transaction != null) {
+            transaction.setStatus(TransactionStatus.SUCCESS);
+            entityManager.merge(transaction);
+        }
+    }
+
+    @Override
+    public void TransactionFailed(UUID transactionId) {
+        Transaction transaction = entityManager.find(Transaction.class, transactionId);
+        if (transaction != null) {
+            transaction.setStatus(TransactionStatus.FAILED);
+            entityManager.merge(transaction);
+        }
+    }
+
+    @Override
+    public List<Transaction> getTransactionsByAccountId(UUID accountId) {
+        return entityManager.createQuery(
+                "SELECT t FROM Transaction t WHERE t.from_account_id = :accountId or t.to_account_id = :accountId", Transaction.class)
+                .setParameter("accountId", accountId)
+                .getResultList();
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return entityManager.createQuery("select t FROM Transaction t", Transaction.class).getResultList();
+    }
+
+    @Override
+    public Transaction getTransactionById(UUID transactionId) {
+        return entityManager.find(Transaction.class, transactionId);
+    }
+}
